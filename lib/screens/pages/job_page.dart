3eec89f3cc2c04c802ec15e6d927b23fa3ccd8ef
mattheuhtn <mattheuhtn@gmail.com>
2022-06-15 13:49:46 +0700

    chore: pagejob post

--- lib/screens/pages/job_page.dart
+++ lib/screens/pages/job_page.dart
@@ -77,6 +77,204 @@ class _JobPageState extends State<JobPage> {
   }
 }
 
+class ApplyJobPost extends StatelessWidget {
+  const ApplyJobPost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        color: Colors.white,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Row(
+            children: [
+              const CircleAvatar(
+                radius: 27,
+                backgroundColor: darkGrayColor,
+                child: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage(
+                    'images/logo_r2s.png',
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Text(
+                'Công ty cổ phần R2S',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Spacer(),
+              InkWell(
+                onTap: () {},
+                child: const Icon(
+                  Icons.bookmark,
+                  // color: darkGrayColor,
+                  color: yellowColor,
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            'Junior UI/UX Designer',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Row(
+            children: const [
+              Icon(
+                Icons.location_on,
+                color: darkGrayColor,
+              ),
+              Text(
+                '1162 Pham Van Dong, Thu Duc',
+                style: TextStyle(color: darkGrayColor),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: const [
+                  HashTag(),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  HashTag(),
+                ],
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 5),
+                  child: const Text(
+                    '\$4K',
+                    style: TextStyle(
+                        color: darkBlueColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class CareJobPost extends StatelessWidget {
+  const CareJobPost({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        color: Colors.white,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Row(
+            children: [
+              const CircleAvatar(
+                radius: 27,
+                backgroundColor: darkGrayColor,
+                child: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage(
+                    'images/logo_r2s.png',
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Text(
+                'Công ty cổ phần R2S',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Spacer(),
+              InkWell(
+                onTap: () {},
+                child: const Icon(
+                  Icons.bookmark,
+                  // color: darkGrayColor,
+                  color: yellowColor,
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            'Junior UI/UX Designer',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Row(
+            children: const [
+              Icon(
+                Icons.location_on,
+                color: darkGrayColor,
+              ),
+              Text(
+                '1162 Pham Van Dong, Thu Duc',
+                style: TextStyle(color: darkGrayColor),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: const [
+                  HashTag(),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  HashTag(),
+                ],
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 5),
+                  child: const Text(
+                    '\$4K',
+                    style: TextStyle(
+                        color: darkBlueColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
 class ApplyJobs extends StatelessWidget {
   const ApplyJobs({super.key});
 
@@ -86,20 +284,13 @@ class ApplyJobs extends StatelessWidget {
       child: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
+          child: Wrap(
+            runSpacing: 10,
             children: const [
-              PostCard(),
-              SizedBox(
-                height: 10,
-              ),
-              PostCard(),
-              SizedBox(
-                height: 10,
-              ),
-              PostCard(),
-              SizedBox(
-                height: 10,
-              ),
+              ApplyJobPost(),
+              ApplyJobPost(),
+              ApplyJobPost(),
+              ApplyJobPost(),
             ],
           ),
         ),
@@ -108,8 +299,25 @@ class ApplyJobs extends StatelessWidget {
   }
 }
 
-class CareJobs extends StatelessWidget {
+class CareJobs extends StatefulWidget {
   const CareJobs({super.key});
+
+  @override
+  State<CareJobs> createState() => _CareJobsState();
+}
+
+class _CareJobsState extends State<CareJobs> {
+  var listJob = <CareJobPost>[
+    const CareJobPost(),
+    const CareJobPost(),
+    const CareJobPost(),
+    const CareJobPost(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -117,42 +325,9 @@ class CareJobs extends StatelessWidget {
       child: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: const [
-              PostCard(),
-              SizedBox(
-                height: 10,
-              ),
-              PostCard(),
-              SizedBox(
-                height: 10,
-              ),
-              PostCard(),
-              SizedBox(
-                height: 10,
-              ),
-              PostCard(),
-              SizedBox(
-                height: 10,
-              ),
-              PostCard(),
-              SizedBox(
-                height: 10,
-              ),
-              PostCard(),
-              SizedBox(
-                height: 10,
-              ),
-              PostCard(),
-              SizedBox(
-                height: 10,
-              ),
-              PostCard(),
-              SizedBox(
-                height: 10,
-              ),
-              PostCard(),
-            ],
+          child: Wrap(
+            runSpacing: 10,
+            children: listJob,
           ),
         ),
       ),
