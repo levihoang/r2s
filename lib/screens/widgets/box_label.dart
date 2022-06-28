2ea4ac9f7515f68229b2d5db3612e252f8a5615e
mattheuhtn <mattheuhtn@gmail.com>
2022-06-28 15:07:19 +0700

    style: boxlabel_validate

--- lib/screens/widgets/box_label.dart
+++ lib/screens/widgets/box_label.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:it_intership_jobs_r2s/utils/colors.dart';
 
 abstract class BoxLabel extends StatefulWidget {
   const BoxLabel({
     super.key,
-    this.prefixIcon,
-    this.suffixIcon,
     this.iconColor,
     this.hintText,
     this.announcementColor,
@@ -15,17 +14,20 @@ abstract class BoxLabel extends StatefulWidget {
     this.verticalPadding,
     this.horizontalPadding,
     this.sizeText,
+    this.title,
+    this.suffixIcon,
   });
+  final String? title;
   final double? verticalPadding;
   final double? horizontalPadding;
-  final IconButton? prefixIcon;
-  final IconButton? suffixIcon;
+  final IconButton? Function()? suffixIcon;
   final Color? borderColor;
   final Color? iconColor;
   final String? hintText;
   final Color? announcementColor;
   final bool? obscure;
   final double? sizeText;
+
   String? announcement(String value);
   final TextEditingController textEditingController;
 
@@ -39,6 +41,13 @@ class BoxLabelState extends State<BoxLabel> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        widget.title == null
+            ? Container()
+            : Text(widget.title ?? '',
+                style: GoogleFonts.openSans(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                )),
         TextField(
           onChanged: (value) {
             setState(() {});
@@ -46,18 +55,20 @@ class BoxLabelState extends State<BoxLabel> {
           obscureText: widget.obscure ?? false,
           controller: widget.textEditingController,
           decoration: InputDecoration(
-            prefixIcon: widget.prefixIcon,
+            // prefixIcon: widget.prefixIcon,
             contentPadding: EdgeInsets.symmetric(
                 horizontal: widget.horizontalPadding ?? 20,
                 vertical: widget.verticalPadding ?? 0),
-            suffixIcon: widget.suffixIcon,
+            suffixIcon: widget.suffixIcon == null ? null : widget.suffixIcon!(),
             focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide(color: widget.borderColor ?? yellowColor),
+              borderSide:
+                  BorderSide(color: widget.borderColor ?? darkBlueColor),
             ),
             enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide(color: widget.borderColor ?? yellowColor),
+              borderSide:
+                  BorderSide(color: widget.borderColor ?? darkBlueColor),
             ),
             hintText: widget.hintText,
           ),
@@ -65,10 +76,12 @@ class BoxLabelState extends State<BoxLabel> {
         const SizedBox(
           height: 10,
         ),
-        Text(
-          widget.announcement(widget.textEditingController.text) ?? '',
-          style: TextStyle(color: widget.announcementColor),
-        )
+        widget.textEditingController.text == ''
+            ? Container()
+            : Text(
+                widget.announcement(widget.textEditingController.text) ?? '',
+                style: TextStyle(color: widget.announcementColor ?? redColor),
+              )
       ],
     );
   }
--- lib/validates/validate.dart
+++ lib/validates/validate.dart
@@ -21,11 +21,32 @@ class Validate {
         r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
         r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
         r"{0,253}[a-zA-Z0-9])?)*$";
+
     RegExp regex = RegExp(pattern);
     if (value == null || value.isEmpty || !regex.hasMatch(value)) {
       return true;
     } else {
       return false;
     }
+  }
+
+  static bool invalidateMobile(String? value) {
+    final regExp = RegExp(r'(^(?:[+0]9)?[0-9]{10}$)');
+
+    if (value == null || value.isEmpty || !regExp.hasMatch(value)) {
+      return true;
+    }
+    return false;
+  }
+
+  static bool invalidateOnlyWord(String? value) {
+    RegExp rex = RegExp(r'[!@#<>?":_`~;[\]\\|=+)(*&^%0-9-]');
+    if (value == null) {
+      return true;
+    }
+    if (rex.hasMatch(value)) {
+      return true;
+    }
+    return false;
   }
 }
