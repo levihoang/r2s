51fb9d38b82a583e3fd9011dbb26159d3530877d
mattheuhtn <mattheuhtn@gmail.com>
2022-06-29 10:17:49 +0700

    hotfix: thiếu file nhỏ

---
+++ lib/src/core/model/json/register.dart
@@ -0,0 +1,34 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import '../role.dart';
+
+part 'register.g.dart';
+
+@JsonSerializable()
+class Register {
+  Register(
+      {this.username,
+      this.password,
+      this.confirmPassword,
+      // this.role,
+      this.email});
+  @JsonKey(name: 'username')
+  final String? username;
+
+  @JsonKey(name: 'password')
+  final String? password;
+
+  @JsonKey(name: 'confirmPassword')
+  final String? confirmPassword;
+
+  @JsonKey(name: 'role')
+  final Role? role = Role(id: 3);
+
+  @JsonKey(name: 'email')
+  final String? email;
+
+  factory Register.fromJson(Map<String, dynamic> json) =>
+      _$RegisterFromJson(json);
+
+  Map<String, dynamic> toJson() => _$RegisterToJson(this);
+}
